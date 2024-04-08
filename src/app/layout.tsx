c0043e9f46201b1189d4296acf15b1ebcee32fd4
Shama2702 <sharminjahan2702@gmail.com>
2024-04-09 00:24:03 +0600

    live chat added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import BasicLayout from "@/components/layout/BasicLayout";
 import { Toaster } from "react-hot-toast";
 import AppStart from "@/components/AppStart";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className + " min-h-screen"}>
+        <Script id="tawk" strategy="lazyOnload">
+          {`
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/66143176a0c6737bd129a522/1hqve2e2a';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+  `}
+        </Script>
         <AppStart />
         <Toaster />
         <BasicLayout>{children}</BasicLayout>
